08c7edb73aafd9f50bfc0d64c28d106a146a7c73
Amarjeet <amarjeet.btechcse@gmail.com>
2024-11-10 19:28:48 +0530

    update it on 20101124

--- 22. Linked List (Part 1)/AddPrint_linkedList.java
+++ 22. Linked List (Part 1)/AddPrint_linkedList.java
@@ -11,9 +11,12 @@ public class AddPrint_linkedList {
 
     public Node head;
     public Node tail;
+    public int size;
 
     public void addFirst(int data) {
         Node newNode = new Node(data); // Moved up to define newNode before use
+
+        size++;
 
         if (head == null) {
             head = tail = newNode; // Now uses the initialized newNode
@@ -26,6 +29,7 @@ public class AddPrint_linkedList {
 
     public void addLast(int data) {
         Node newNode = new Node(data);
+        size++;
         if (head == null) {
             head = tail = newNode;
             return;
@@ -41,6 +45,7 @@ public class AddPrint_linkedList {
             return;
         }
         Node newNode = new Node(data);
+        size++;
         Node temp = head;
         int i = 0;
 
@@ -52,6 +57,45 @@ public class AddPrint_linkedList {
         //i = index-1 ; temp -> previous
         newNode.next = temp.next;
         temp.next = newNode;
+    }
+
+    public int removeFirst() {
+        if(size == 0) {
+            System.out.println("LL is empty.");
+            return Integer.MIN_VALUE;
+        }else if(size == 1) {
+            int val = head.data;
+            head = tail = null;
+            size = 0;
+            return val;
+        }
+        int val = head.data;
+        head =head.next;
+        size--;
+        return val;
+    }
+
+    public int removeLast() {
+        if(size == 0) {
+            System.out.println("LL is empty.");
+            return Integer.MAX_VALUE;
+        }else if(size == 1) {
+            int val = head.data;
+            head = tail = null;
+            size = 0;
+            return val;
+        }
+        // prev : i = size-2
+        Node prev = head;
+        for(int i =0; i<size-2; i++){
+            prev = prev.next;
+        }
+
+        int val = prev.next.data; //tail.data
+        prev.next = null;
+        tail = prev;
+         size--;
+         return val;
     }
 
     public void print() {
@@ -85,8 +129,17 @@ public class AddPrint_linkedList {
         ll.addLast(4);
         ll.print();
 
-        ll.addMiddle(2, 10);
+        ll.addMiddle(2, 5);
         ll.print();
+
+        System.out.println("Total size is :"+ll.size);
+
+        ll.removeFirst();
+        ll.print();
+
+        ll.removeLast();
+        ll.print();
+        System.out.println("Total size is :"+ll.size);
+
     }
 }
-
