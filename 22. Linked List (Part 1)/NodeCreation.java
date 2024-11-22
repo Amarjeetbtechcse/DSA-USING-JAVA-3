209d6dc6fad4ff6d257f554d042b3f57d88b0a2f
Amarjeet <amarjeet.btechcse@gmail.com>
2024-11-22 20:22:03 +0530

    221124

--- 22. Linked List (Part 1)/NodeCreation.java
+++ 22. Linked List (Part 1)/NodeCreation.java
@@ -8,8 +8,6 @@ public class NodeCreation{
             this.next = null;
         }
     }
-    
-    public static void main(String[] args) {
-        
+        public static void main(String[] args) {
     }
 }
